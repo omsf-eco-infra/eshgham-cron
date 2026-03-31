14a900d3372020bf05c48c99820d3211082d6c8d
David W.H. Swenson <david.swenson@omsf.io>
2026-03-31 11:09:28 -0500

    Use uri_with_digest for notification image
    This should fix some problems we occasionally have in deployment.

--- outputs.tf
+++ outputs.tf
@@ -4,12 +4,12 @@ output "lambda_republished_image_uri" {
 }
 
 output "notification_republished_image_uri" {
-  description = "Private ECR image URI for the republished notification image (null when notification_image_uri_override is used)."
-  value       = local.use_notification_image_override ? null : module.notification_image_republish[0].lambda_image_uri
+  description = "Private ECR image URI with digest for the republished notification image (null when notification_image_uri_override is used)."
+  value       = local.use_notification_image_override ? null : module.notification_image_republish[0].lambda_image_uri_with_digest
 }
 
 output "notification_image_uri" {
-  description = "Notification image URI used by notification lambdas (republished image or notification_image_uri_override)."
+  description = "Notification image URI used by notification lambdas (republished image with digest or notification_image_uri_override)."
   value       = local.notification_lambda_image_uri
 }
 
